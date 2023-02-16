7934310d88f6b757a7c01702a00ad128132a2667
Rajput-surendra <surendraalphawizz@gmail.com>
2023-02-16 19:15:56 +0530

    Initial 13/2/23

---
+++ android/app/src/main/java/com/ziberto/MainActivity.java
@@ -0,0 +1,6 @@
+package com.alpha.canteenuser;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,20 @@
+final String appName = 'Canteen';
+
+final String packageName = 'com.alpha.canteenuser';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.alpha.canteenuser';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://eshopmultivendor.page.link';
+final String deepLinkName = 'Canteen';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+//final String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+// final String baseUrl = 'https://alphawizztest.tk/Food On The Way_Ecom/app/v1/api/';
+final String baseUrl = 'https://canteenfood.co.in/app/v1/api/';
+final String imageUrl = 'https://canteenfood.co.in/';
+final String jwtKey = "0352e7a815f965e8e278d47976b8f9cf466e1f42";
